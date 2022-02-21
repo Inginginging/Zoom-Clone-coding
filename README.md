c25b1f2e680f731e24f65516676382a93e1cd71e
ing928 <kik928@nate.com>
2022-02-21 21:19:54 +0900

    Server Setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Zoom-Clone
+
+Using
+
+1. NodeJs
+2. WebRTC
+3. Websockets
---
+++ src/server.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+
+console.log("Listening on Port 3000");
+
+app.listen(3000);
