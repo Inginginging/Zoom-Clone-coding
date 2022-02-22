2ebc60d602e2d92aa1a223fadd71cb83e2acd742
ing928 <kik928@nate.com>
2022-02-22 21:32:26 +0900

    Merge Protocol http+ws

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http"; //nodeJs 에 내장돼 있음.
+import { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -6,9 +8,12 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public")); //frontend code 보이게 하기.
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 
-app.listen(3000, handleListen);
+const server = http.createServer(app); //express로 만든 server에 접근.
+const wss = new WebSocketServer({ server }); //http server위에 ws server를 올림. => 같은 포트에서 두가지 프로토콜 이해 가능.
+
+server.listen(3000, handleListen);
