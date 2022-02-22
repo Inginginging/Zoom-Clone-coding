14dcf69917bcc2bbf34d49a0d808dd4a7cdd335d
ing928 <kik928@nate.com>
2022-02-22 23:10:20 +0900

    socket event (server와 browser간의 실시간통신)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,13 @@
 const socket = new WebSocket(`ws://${window.location.host}`); //connection된 server의 socket
+
+//socket의 event handler. bakend로 부터 영향을 받음. browser console에 보임.
+socket.addEventListener("open", () => console.log("Connected to Server :)"));
+socket.addEventListener("message", (message) =>
+  console.log(`New Message from Server: ${message.data}`)
+);
+socket.addEventListener("close", () =>
+  console.log("Disconnected to Server :(")
+);
+
+//backend에 영향을 주는 eventhandler. backend console에 보임
+setTimeout(() => socket.send("Hello From Browser"), 10000);
--- src/server.js
+++ src/server.js
@@ -16,10 +16,11 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const server = http.createServer(app); //express로 만든 server에 접근.
 const wss = new WebSocketServer({ server }); //http server위에 ws server를 올림. => 같은 포트에서 두가지 프로토콜 이해 가능.
 
-const handleConnection = (socket) => {
-  //연결된 browser의 socket
-  console.log(socket);
-};
-wss.on("connection", handleConnection); //connection event가 일어나면 callback fn 호출
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser :)");
+  socket.on("close", () => console.log("DisConnected to Browser :("));
+  socket.on("message", (message) => console.log(message.toString("utf-8"))); //front로 부터 받은 message
+  socket.send("Hello From Server"); //browser로 보내는 message
+});
 
 server.listen(3000, handleListen);
